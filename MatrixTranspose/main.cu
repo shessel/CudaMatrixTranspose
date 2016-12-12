2f0ed5a6a0c025cf39b3d3748d56cb26b2d09a48
shessel <shessel@users.noreply.github.com>
2016-12-12 19:25:27 +0100

    put back in one file, because build ignores kernels.cuh if not cleaning each time

--- MatrixTranspose/main.cu
+++ MatrixTranspose/main.cu
@@ -11,7 +11,113 @@
 
 #include "Matrix.h"
 #include "CudaHelpers.h"
-#include "kernels.cuh"
+
+const unsigned int TILE_SIZE_X = 16;
+const unsigned int TILE_SIZE_Y = TILE_SIZE_X;
+const unsigned int DIM_X = 1024;
+const unsigned int DIM_Y = DIM_X;
+const unsigned int NUM_REPS = 100;
+
+template <typename T>
+__global__ void baseLineCopy(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+
+	out[idxIn] = in[idxIn];
+}
+
+template <typename T>
+__global__ void baseLineCopyShared(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+
+	cache[threadIdx.y][threadIdx.x] = in[idxIn];
+	__syncthreads();
+
+	out[idxIn] = cache[threadIdx.y][threadIdx.x];
+}
+
+template <typename T>
+__global__ void naiveTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	for (unsigned int j = 0; j < height; j++) {
+		for (unsigned int i = 0; i < width; i++) {
+			auto idxIn = j * width + i;
+			auto idxOut = i * height + j;
+			out[idxOut] = in[idxIn];
+		}
+	}
+}
+
+template <typename T>
+__global__ void naiveParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	unsigned int idxIn = blockIdx.x * width + threadIdx.x;
+	unsigned int idxOut = threadIdx.x * height + blockIdx.x;
+
+	out[idxOut] = in[idxIn];
+}
+
+template <typename T>
+__global__ void naiveBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+	unsigned int idxOut = xIndex * height + yIndex;
+
+	out[idxOut] = in[idxIn];
+}
+
+template <typename T>
+__global__ void naiveSharedBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+	unsigned int idxOut = xIndex * height + yIndex;
+
+	cache[threadIdx.y][threadIdx.x] = in[idxIn];
+	__syncthreads();
+
+	out[idxOut] = cache[threadIdx.y][threadIdx.x];
+}
+
+template <typename T>
+__global__ void coalescedSharedBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+
+	// write out consecutively and do transposing by swapping x and y when reading from shared memory
+	unsigned int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
+	unsigned int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
+	unsigned int idxOut = yIndexOut * height + xIndexOut;
+
+	cache[threadIdx.y][threadIdx.x] = in[idxIn];
+	__syncthreads();
+
+	out[idxOut] = cache[threadIdx.x][threadIdx.y];
+}
+
+template <typename T>
+__global__ void coalescedSharedBlockWiseParallelTransposeNoBankConflicts(const T * const in, T * const out, unsigned int width, unsigned int height) {
+	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X + 1];
+	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int idxIn = yIndex * width + xIndex;
+
+	// write out consecutively and do transposing by swapping x and y when reading from shared memory
+	unsigned int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
+	unsigned int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
+	unsigned int idxOut = yIndexOut * height + xIndexOut;
+
+	cache[threadIdx.y][threadIdx.x] = in[idxIn];
+	__syncthreads();
+
+	out[idxOut] = cache[threadIdx.x][threadIdx.y];
+}
 
 template <typename T, unsigned int WIDTH, unsigned int HEIGHT>
 void transposeCpu(const Matrix<T, WIDTH, HEIGHT>& in, Matrix<T, HEIGHT, WIDTH>& out) {
@@ -77,7 +183,40 @@ float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsign
 template <typename T>
 float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsigned int),
 				  const KernelParams<T>& params, unsigned int numReps) {
-	return averageTime(kernel, params.d_in, params.d_out, params.width, params.height, params.gridDim, params.blockDim, numReps);
+	float kernelTime = -1.0f;
+	cudaEvent_t start;
+	cudaEvent_t stop;
+	try {
+		checkCudaStatus(cudaEventCreate(&start));
+		checkCudaStatus(cudaEventCreate(&stop));
+		// Clear error status
+		checkCudaStatus(cudaGetLastError());
+
+		// warmup to avoid timing startup
+		kernel << <params.gridDim, params.blockDim >> > (params.d_in, params.d_out, params.width, params.height);
+
+		checkCudaStatus(cudaEventRecord(start, 0));
+
+		for (unsigned int i = 0; i < numReps; i++)
+		{
+			kernel << <params.gridDim, params.blockDim >> > (params.d_in, params.d_out, params.width, params.height);
+			// Ensure no launch failure
+			checkCudaStatus(cudaGetLastError());
+		}
+
+		checkCudaStatus(cudaEventRecord(stop, 0));
+		checkCudaStatus(cudaEventSynchronize(stop));
+		checkCudaStatus(cudaEventElapsedTime(&kernelTime, start, stop));
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
+	catch (CudaException e) {
+		std::cerr << "Kernel launch failed: " << e.what() << std::endl;
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
+	}
+
+	return kernelTime / numReps;
 }
 
 void printStatistics(float avgTime, size_t mem_size) {
