389483c01f8461497ddcd13840811581f518b919
shessel <shessel@users.noreply.github.com>
2016-12-15 01:11:30 +0100

    move to signed int, add kernel names to output, improve coalesced kernels

--- MatrixTranspose/main.cu
+++ MatrixTranspose/main.cu
@@ -12,17 +12,17 @@
 #include "Matrix.h"
 #include "CudaHelpers.h"
 
-const unsigned int TILE_SIZE_X = 16;
-const unsigned int TILE_SIZE_Y = TILE_SIZE_X;
-const unsigned int DIM_X = 1024;
-const unsigned int DIM_Y = DIM_X;
-const unsigned int NUM_REPS = 100;
+const int TILE_SIZE_X = 16;
+const int TILE_SIZE_Y = TILE_SIZE_X;
+const int DIM_X = 1024;
+const int DIM_Y = DIM_X;
+const int NUM_REPS = 100;
 
 template <typename T>
-__global__ void baseLineCopy(const T * const in, T * const out, unsigned int width, unsigned int height) {
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+__global__ void baseLineCopy(const T * const in, T * const out, int width, int height) {
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
 
 	if (idxIn < width * height) {
 		out[idxIn] = in[idxIn];
@@ -30,11 +30,11 @@ __global__ void baseLineCopy(const T * const in, T * const out, unsigned int wid
 }
 
 template <typename T>
-__global__ void baseLineCopyShared(const T * const in, T * const out, unsigned int width, unsigned int height) {
+__global__ void baseLineCopyShared(const T * const in, T * const out, int width, int height) {
 	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
 
 	if (idxIn < width * height) {
 		cache[threadIdx.y][threadIdx.x] = in[idxIn];
@@ -45,9 +45,9 @@ __global__ void baseLineCopyShared(const T * const in, T * const out, unsigned i
 }
 
 template <typename T>
-__global__ void naiveTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
-	for (unsigned int j = 0; j < height; j++) {
-		for (unsigned int i = 0; i < width; i++) {
+__global__ void naiveTranspose(const T * const in, T * const out, int width, int height) {
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
 			auto idxIn = j * width + i;
 			auto idxOut = i * height + j;
 			out[idxOut] = in[idxIn];
@@ -56,93 +56,93 @@ __global__ void naiveTranspose(const T * const in, T * const out, unsigned int w
 }
 
 template <typename T>
-__global__ void naiveParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
-	unsigned int idxIn = blockIdx.x * width + threadIdx.x;
+__global__ void naiveParallelTranspose(const T * const in, T * const out, int width, int height) {
+	int idxIn = blockIdx.x * width + threadIdx.x;
 
 	if (idxIn < width * height) {
-		unsigned int idxOut = threadIdx.x * height + blockIdx.x;
+		int idxOut = threadIdx.x * height + blockIdx.x;
 		out[idxOut] = in[idxIn];
 	}
 }
 
 template <typename T>
-__global__ void naiveBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+__global__ void naiveBlockWiseParallelTranspose(const T * const in, T * const out, int width, int height) {
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
 
 	if (xIndex < width && yIndex < height) {
-		unsigned int idxOut = xIndex * height + yIndex;
+		int idxOut = xIndex * height + yIndex;
 		out[idxOut] = in[idxIn];
 	}
 }
 
 template <typename T>
-__global__ void naiveSharedBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+__global__ void naiveSharedBlockWiseParallelTranspose(const T * const in, T * const out, int width, int height) {
 	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
 
 	if (xIndex < width && yIndex < height) {
 		cache[threadIdx.y][threadIdx.x] = in[idxIn];
 		__syncthreads();
 
-		unsigned int idxOut = xIndex * height + yIndex;
+		int idxOut = xIndex * height + yIndex;
 		out[idxOut] = cache[threadIdx.y][threadIdx.x];
 	}
 }
 
 template <typename T>
-__global__ void coalescedSharedBlockWiseParallelTranspose(const T * const in, T * const out, unsigned int width, unsigned int height) {
+__global__ void coalescedSharedBlockWiseParallelTranspose(const T * const in, T * const out, int width, int height) {
 	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X];
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
+
+	// write out consecutively (with increasing threadIdx.x) and do
+	// transposing by swapping x and y when reading from shared memory
+	int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
+	int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
+	int idxOut = yIndexOut * height + xIndexOut;
 
 	if (xIndex < width && yIndex < height) {
-		// write out consecutively (with increasing threadIdx.x) and do
-		// transposing by swapping x and y when reading from shared memory
-		unsigned int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
-		unsigned int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
-		unsigned int idxOut = yIndexOut * height + xIndexOut;
-
 		cache[threadIdx.y][threadIdx.x] = in[idxIn];
-		__syncthreads();
+	}
+	__syncthreads();
 
-		if (xIndexOut < width && yIndexOut < height) {
-			out[idxOut] = cache[threadIdx.x][threadIdx.y];
-		}
+	if (xIndexOut < width && yIndexOut < height) {
+		out[idxOut] = cache[threadIdx.x][threadIdx.y];
 	}
 }
 
 template <typename T>
-__global__ void coalescedSharedBlockWiseParallelTransposeNoBankConflicts(const T * const in, T * const out, unsigned int width, unsigned int height) {
+__global__ void coalescedSharedBlockWiseParallelTransposeNoBankConflicts(const T * const in, T * const out, int width, int height) {
 	__shared__ T cache[TILE_SIZE_Y][TILE_SIZE_X + 1];
-	unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-	unsigned int idxIn = yIndex * width + xIndex;
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	int idxIn = yIndex * width + xIndex;
+
+	// write out consecutively (with increasing threadIdx.x) and do
+	// transposing by swapping x and y when reading from shared memory
+	int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
+	int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
+	int idxOut = yIndexOut * height + xIndexOut;
 
 	if (xIndex < width && yIndex < height) {
-		// write out consecutively (with increasing threadIdx.x) and do
-		// transposing by swapping x and y when reading from shared memory
-		unsigned int xIndexOut = blockIdx.y * blockDim.y + threadIdx.x;
-		unsigned int yIndexOut = blockIdx.x * blockDim.x + threadIdx.y;
-		unsigned int idxOut = yIndexOut * height + xIndexOut;
-
 		cache[threadIdx.y][threadIdx.x] = in[idxIn];
-		__syncthreads();
+	}
+	__syncthreads();
 
-		if (xIndexOut < width && yIndexOut < height) {
-			out[idxOut] = cache[threadIdx.x][threadIdx.y];
-		}
+	if (xIndexOut < width && yIndexOut < height) {
+		out[idxOut] = cache[threadIdx.x][threadIdx.y];
 	}
 }
 
-template <typename T, unsigned int WIDTH, unsigned int HEIGHT>
+template <typename T, int WIDTH, int HEIGHT>
 void transposeCpu(const Matrix<T, WIDTH, HEIGHT>& in, Matrix<T, HEIGHT, WIDTH>& out) {
-	for (unsigned int j = 0; j < HEIGHT; j++) {
-		for (unsigned int i = 0; i < WIDTH; i++) {
+	for (int j = 0; j < HEIGHT; j++) {
+		for (int i = 0; i < WIDTH; i++) {
 			auto inIndex = j * WIDTH + i;
 			auto outIndex = i * HEIGHT + j;
 			out[outIndex] = in[inIndex];
@@ -154,16 +154,16 @@ template<typename T>
 struct KernelParams {
 	const T * const d_in;
 	T * const d_out;
-	unsigned int width;
-	unsigned int height;
+	int width;
+	int height;
 	dim3 gridDim;
 	dim3 blockDim;
 };
 
 template <typename T>
-float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsigned int),
-				  const T * const in, T * const out, unsigned int width, unsigned int height,
-				  dim3 grid, dim3 block, unsigned int numReps) {
+float averageTime(void(*kernel)(const T * const, T * const, int, int),
+				  const T * const in, T * const out, int width, int height,
+				  dim3 grid, dim3 block, int numReps) {
 	float kernelTime = -1.0f;
 	cudaEvent_t start;
 	cudaEvent_t stop;
@@ -178,7 +178,7 @@ float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsign
 
 		checkCudaStatus(cudaEventRecord(start, 0));
 
-		for (unsigned int i = 0; i < numReps; i++)
+		for (int i = 0; i < numReps; i++)
 		{
 			kernel << <grid, block >> > (in, out, width, height);
 			// Ensure no launch failure
@@ -202,8 +202,8 @@ float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsign
 }
 
 template <typename T>
-float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsigned int),
-				  const KernelParams<T>& params, unsigned int numReps) {
+float averageTime(void(*kernel)(const T * const, T * const, int, int),
+				  const KernelParams<T>& params, int numReps) {
 	float kernelTime = -1.0f;
 	cudaEvent_t start;
 	cudaEvent_t stop;
@@ -218,7 +218,7 @@ float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsign
 
 		checkCudaStatus(cudaEventRecord(start, 0));
 
-		for (unsigned int i = 0; i < numReps; i++)
+		for (int i = 0; i < numReps; i++)
 		{
 			kernel << <params.gridDim, params.blockDim >> > (params.d_in, params.d_out, params.width, params.height);
 			// Ensure no launch failure
@@ -241,34 +241,35 @@ float averageTime(void(*kernel)(const T * const, T * const, unsigned int, unsign
 }
 
 void printStatistics(float avgTime, size_t mem_size) {
-	static const unsigned int BYTES_PER_GIGABYTE = 1024 * 1024 * 1024;
+	static const int BYTES_PER_GIGABYTE = 1024 * 1024 * 1024;
 	// (1 read + 1 write) * (size in GB) / (time in sec)
 	float kernelBandwidth = (2.0f * mem_size / (BYTES_PER_GIGABYTE)) / (avgTime / 1000.0f);
 	std::cout << avgTime << " " << mem_size << " " << kernelBandwidth << std::endl;
 }
 
-template <typename T, unsigned int WIDTH, unsigned int HEIGHT>
+template <typename T, int WIDTH, int HEIGHT>
 void compare(const Matrix<T, HEIGHT, WIDTH>& toCompare, const Matrix<T, HEIGHT, WIDTH>& groundTruth) {
 	std::cout << (toCompare == groundTruth ? "Test Passed" : "Test Failed") << std::endl;
 }
 
 template <typename T>
-void testKernel(void(*kernel)(const T * const, T * const, unsigned int, unsigned int),
-				const KernelParams<T>& params, unsigned int numReps) {
+void testKernel(const char * const name, void(*kernel)(const T * const, T * const, int, int),
+				const KernelParams<T>& params, int numReps) {
+	std::cout << name << ": ";
 	float avgTime = averageTime(kernel, params, numReps);
 	printStatistics(avgTime, params.width * params.height * sizeof(T));
 }
 
-template <typename T, unsigned int WIDTH, unsigned int HEIGHT >
-void testKernel(void(*kernel)(const T * const, T * const, unsigned int, unsigned int),
-				const KernelParams<T>& params, unsigned int numReps,
+template <typename T, int WIDTH, int HEIGHT >
+void testKernel(const char * const name, void(*kernel)(const T * const, T * const, int, int),
+				const KernelParams<T>& params, int numReps,
 				Matrix<T, WIDTH, HEIGHT>& out, const Matrix<T, WIDTH, HEIGHT>& groundTruth) {
-	testKernel(kernel, params, numReps);
+	testKernel(name, kernel, params, numReps);
 	cudaMemcpy(out.getData(), params.d_out, WIDTH * HEIGHT * sizeof(T), cudaMemcpyDefault);
 	compare(out, groundTruth);
 }
 
-template <typename T, unsigned int WIDTH, unsigned int HEIGHT>
+template <typename T, int WIDTH, int HEIGHT>
 void transposeGpu(const Matrix<T, WIDTH, HEIGHT>& in, Matrix<T, HEIGHT, WIDTH>& out, const Matrix<T, HEIGHT, WIDTH>& groundTruth) {
 	cudaSetDevice(0);
 	T* d_in;
@@ -283,29 +284,29 @@ void transposeGpu(const Matrix<T, WIDTH, HEIGHT>& in, Matrix<T, HEIGHT, WIDTH>& 
 
 	KernelParams<T> kernelParams = { d_in, d_out, WIDTH, HEIGHT, gridTiled, blockTiled };
 
-	testKernel(baseLineCopy, kernelParams, NUM_REPS);
-	testKernel(baseLineCopyShared, kernelParams, NUM_REPS);
+	testKernel("baseLineCopy", baseLineCopy, kernelParams, NUM_REPS);
+	testKernel("baseLineCopyShared", baseLineCopyShared, kernelParams, NUM_REPS);
 
 	if (WIDTH * HEIGHT < 4096) {
 		dim3 gridOne(1);
 		dim3 blockOne(1);
 		kernelParams.gridDim = gridOne;
 		kernelParams.blockDim = blockOne;
-		testKernel(naiveTranspose, kernelParams, NUM_REPS, out, groundTruth);
+		testKernel("naiveTranspose", naiveTranspose, kernelParams, NUM_REPS, out, groundTruth);
 	}
 
 	dim3 gridRowWise(HEIGHT);
 	dim3 blockRowWise(WIDTH);
 	kernelParams.gridDim = gridRowWise;
 	kernelParams.blockDim = blockRowWise;
-	testKernel(naiveParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
+	testKernel("naiveParallelTranspose", naiveParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
 
 	kernelParams.gridDim = gridTiled;
 	kernelParams.blockDim = blockTiled;
-	testKernel(naiveBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
-	testKernel(naiveSharedBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
-	testKernel(coalescedSharedBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
-	testKernel(coalescedSharedBlockWiseParallelTransposeNoBankConflicts, kernelParams, NUM_REPS, out, groundTruth);
+	testKernel("naiveBlockWiseParallelTranspose", naiveBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
+	testKernel("naiveSharedBlockWiseParallelTranspose", naiveSharedBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
+	testKernel("coalescedSharedBlockWiseParallelTranspose", coalescedSharedBlockWiseParallelTranspose, kernelParams, NUM_REPS, out, groundTruth);
+	testKernel("coalescedSharedBlockWiseParallelTransposeNoBankConflicts", coalescedSharedBlockWiseParallelTransposeNoBankConflicts, kernelParams, NUM_REPS, out, groundTruth);
 
 	cudaFree(d_in);
 	cudaFree(d_out);
